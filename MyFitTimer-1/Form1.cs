4ce777858937044d14f8cea0c1a9cdccebdf88c5
Jakbrown195 <72112243+Jakbrown195@users.noreply.github.com>
2020-09-29 21:49:14 -0400

    Add files via upload

---
+++ MyFitTimer-1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFitTimer_1
+{
+    public partial class Form1 : Form
+    {
+
+        int sec = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            string msg = "Time " + sec.ToString();
+
+            MessageBox.Show(msg);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sec++;
+        }
+    }
+}
